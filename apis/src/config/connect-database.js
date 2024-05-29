aa0562102d23e4f3bd9f05e2392eb3e3375a1f65
thuat410 <thuat0410@gmail.com>
2024-05-30 02:47:40 +0700

    fix env

---
+++ apis/src/config/connect-database.js
@@ -0,0 +1,16 @@
+const dbHost = process.env.DB_HOST || 'localhost'
+const dbPort = process.env.DB_PORT || 27017
+const dbName = process.env.DB_NAME || 'demo_db'
+
+const mongoUrl = `mongodb://${dbHost}:${dbPort}/${dbName}`
+
+const connectWithRetry = () => {
+    return mongoose.connect(mongoUrl, { useNewUrlParser: true, useFindAndModify: false }, (err) => {
+        if (err) {
+            console.error('Failed to connect to mongo on startup - retrying in 5 sec', err)
+            setTimeout(connectWithRetry, 5000)
+        }
+    }
+}
+
+module.exports = connectWithRetry
